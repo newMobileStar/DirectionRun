d476a776c926b625817081b0d57e9e6eb1a2dee9
New Star <newstar@news-mac.local>
2016-01-19 00:44:45 -0800

    Simple example for running default map for direction

--- DirectionRun/ViewController.m
+++ DirectionRun/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
+
 
 @interface ViewController ()
 
@@ -17,6 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)onClickDirectionBtn:(id)sender {
+    
+    double destinationLatitude = 37.785834;
+    double destinationLongitude = -122.406417;
+    
+    CLLocationCoordinate2D destinationLocation = CLLocationCoordinate2DMake(destinationLatitude,destinationLongitude);
+    MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate:destinationLocation addressDictionary:nil];
+    MKMapItem *item = [[MKMapItem alloc] initWithPlacemark:placemark];
+    item.name = @"Destination Location";
+    [item openInMapsWithLaunchOptions:nil];
 }
 
 - (void)didReceiveMemoryWarning {
